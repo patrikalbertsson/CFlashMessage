245f0afbabb82ed7288fb0051662f79b4ce6cde1
Patrik Albertsson <patrik.albertsson@hotmail.com>
2016-03-16 15:08:16 +0100

    First commit

---
+++ src/FlashMessage/CFlashMessage.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace Anax\FlashMessage;
+
+class CFlashMessage implements \Anax\DI\IInjectionAware {
+    
+    use \Anax\DI\Tinjectable;
+ 
+    //
+    // Variables
+    //
+    public $message = null;
+    
+    //
+    // Constructor
+    //
+    public function __construct() {
+        
+        if(!isset($_SESSION['message'])) {
+            $_SESSION['message'] = [];
+        }  
+        
+        $this->message = $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+    
+    //
+    // Set message
+    //
+    public function setFlash($type, $message) {
+        
+        // Valid types: success, notice, warning and error.
+        
+        $flash = ['type' => "flash-" . $type, 'message' => $message];
+        $_SESSION['message'] = $flash;
+        
+    }
+     
+    //
+    // Do session have a message?
+    // 
+    public function hasFlash() {
+        
+        return isset($this->message) && count($this->message) > 0;
+        
+    }
+    
+    //
+    // Get message
+    //
+    public function getFlash() {
+        
+        return $this->message;
+        
+    }
+
+}
